067d0cefe2755ccc22711f821f4c7892dc6e737f
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-04 08:04:55 +0530

    Create Even_loop.c

---
+++ Even_loop.c
@@ -0,0 +1,31 @@
+//Printing even numbers upto given number
+
+#include<stdio.h>
+
+int evenloop(int csk){
+    
+    int i,j;
+    
+    for(i=1;i<=csk;i++){
+        
+        if(i%2==0){
+            
+            printf("%d ",i);
+        }
+    }
+    printf("\n");
+    
+}
+
+int main(){
+    
+    int number;
+    
+    printf("Enter an integer : ");
+    
+    scanf("%d",&number);
+    
+    evenloop(number);
+    
+    return 0;
+}
