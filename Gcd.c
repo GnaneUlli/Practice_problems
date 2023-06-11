859ea92774dc23eef8835f526a4fd80e64b29e3e
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-11 15:39:45 +0530

    Update Gcd.c

--- Gcd.c
+++ Gcd.c
@@ -18,17 +18,17 @@ int gcd(int num1, int num2) {
 
 int main() {
   
-    int number1, number2;
+    int num1, num2;
     
     printf("Enter the first number: ");
   
-    scanf("%d", &number1);
+    scanf("%d", &num1);
     
     printf("Enter the second number: ");
   
-    scanf("%d", &number2);
+    scanf("%d", &num2);
     
-    int result = gcd(number1, number2);
+    int result = gcd(num1, num2);
     
     printf("GCD: %d\n", result);
     
